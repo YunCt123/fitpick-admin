2d17f0d8a65420aa77304e2afc16d5f7ca34246c
YunCt123 <yunct1234@gmail.com>
2025-09-06 19:12:25 +0700

    create sidebar

---
+++ src/pages/Blogs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Blogs: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Blogs</h1>
+      <div className="bg-white p-6 rounded-lg shadow">
+        <p className="text-gray-600">Blogs management will be displayed here.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
