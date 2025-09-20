1d020a7f6c06ea11d1c755d3771de5186bd69e59
YunCt123 <yunct1234@gmail.com>
2025-09-20 23:59:53 +0700

    fix conflict

--- src/components/dashboard/charts/RevenueBar.tsx
+++ src/components/dashboard/charts/RevenueBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
     ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
 } from "recharts";
--- src/pages/DashboardHome.tsx
+++ src/pages/DashboardHome.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { useDashboard } from "../hooks/useDashboard";
 import { StatCard } from "../components/dashboard/StatCard";
 import { UserTypeDonut } from "../components/dashboard/charts/UserTypeDonut";
